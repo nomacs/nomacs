a9c4c9a3211d4b02c7f421392b52afaf4ef43100
Stefan Fiel <stefan@nomacs.org>
2012-11-27 08:36:59 +0100

    oooops, error in last commit

--- ImageLounge/src/DkNoMacs.cpp
+++ ImageLounge/src/DkNoMacs.cpp
@@ -1944,8 +1944,8 @@ void DkNoMacs::openImgManipulationDialog() {
 		imgManipulationDialog = new DkImageManipulationDialog(this);
 	else imgManipulationDialog->resetValues();
 
-	QImage* tmpImg = viewport()->getImageLoader()->getImage();
-	imgManipulationDialog->setImage(tmpImg);
+	QImage tmpImg = viewport()->getImageLoader()->getImage();
+	imgManipulationDialog->setImage(&tmpImg);
 
 	bool done = imgManipulationDialog->exec();
 
