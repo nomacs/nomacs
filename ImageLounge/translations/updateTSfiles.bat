84da16ddfe1d44c1d6a33d751e1e75632b0fdd5c
diemmarkus <diemmarkus@gmail.com>
2013-04-04 13:03:02 +0200

    path updated

--- ImageLounge/translations/updateTSfiles.bat
+++ ImageLounge/translations/updateTSfiles.bat
@@ -1,4 +1,4 @@
-set lupdatePath="C:\Qt\4.8.2\qt-everywhere-opensource-src-4.8.2\bin\lupdate.exe"
+set lupdatePath="C:\Qt\qt-everywhere-opensource-src-4.8.2-msvc2012-x86\bin\lupdate.exe"
 
 for %%f in (*.ts) DO %lupdatePath%  ..\src\ -ts %%f
 pause
