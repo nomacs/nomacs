37f5f6a3a983930d23b8440d88b88fb17317b2e0
Stefan Fiel <stefan@nomacs.org>
2013-10-14 11:47:38 +0200

    removed size_t (warning) added defines for MAC_OS

--- ImageLounge/src/DkWidgets.cpp
+++ ImageLounge/src/DkWidgets.cpp
@@ -542,7 +542,7 @@ void DkFilePreview::mouseMoveEvent(QMouseEvent *event) {
 			if (worldMatrix.mapRect(thumbRects.at(idx)).contains(event->pos())) {
 				selected = idx;
 
-				if ((size_t)selected <= thumbPool->getThumbs().size() && selected >= 0) {
+				if (selected <= thumbPool->getThumbs().size() && selected >= 0) {
 					QSharedPointer<DkThumbNailT> thumb = thumbPool->getThumbs().at(selected);
 					createSelectedEffect(thumb->getImage(), DkSettings::display.highlightColor);
 				
