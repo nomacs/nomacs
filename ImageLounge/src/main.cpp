ae059a897ffa28f71e21a2b5660deef40140ce7b
Stefan Fiel <fiel@caa.tuwien.ac.at>
2014-09-11 07:48:30 +0200

    fixed for Qt5

--- ImageLounge/src/main.cpp
+++ ImageLounge/src/main.cpp
@@ -144,7 +144,8 @@ int main(int argc, char *argv[]) {
 	qDebug() << "\n";
 	// DEBUG --------------------------------------------------------------------
 
-	qDebug() << "data location: " << QDir(QDesktopServices::storageLocation(QDesktopServices::DataLocation)).absolutePath();
+// 	qDebug() << "data location: " << QDir(QDesktopServices::storageLocation(QDesktopServices::DataLocation)).absolutePath();
+	qDebug() << "data location: " << QDir(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + "/data/" + QCoreApplication::organizationName() + "/" + QCoreApplication::applicationName());
 	//QSettings settings;
 
 	QString translationName = "nomacs_"+ settings.value("GlobalSettings/language", nmc::DkSettings::global.language).toString() + ".qm";
