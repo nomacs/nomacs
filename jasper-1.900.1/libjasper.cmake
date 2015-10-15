793fed2cbaf62893d525ff575f7c4a717ebdae5b
diemmarkus <diemmarkus@gmail.com>
2015-10-15 11:58:49 +0200

    JPEG 2000 removed -> it's now in qt5

--- jasper-1.900.1/libjasper.cmake
+++
@@ -1,3 +0,0 @@
-SET(LIBJASPER_DEBUG_LIB C:/VSProjects/nomacs-plugins/build2012x86/jasper-1.900.1/Debug/libjasper.lib)
-SET(LIBJASPER_RELEASE_LIB C:/VSProjects/nomacs-plugins/build2012x86/jasper-1.900.1/Release/libjasper.lib)
-SET(LIBJASPER_INCLUDE_DIRECTORY C:/VSProjects/nomacs-plugins/jasper-1.900.1/src/libjasper/include/)
