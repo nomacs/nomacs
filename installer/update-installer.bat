ad183f0b30c1cec0cd88286b9c322327ffead5d6
diemmarkus <diemmarkus@gmail.com>
2015-12-15 11:27:53 +0100

    certificates

--- installer/update-installer.bat
+++ installer/update-installer.bat
@@ -1,6 +1,11 @@
 REM dir to current directory
 cd %~dp0
 
+REM sign exe
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /f C:\VSProjects\TU-code-signing.p12  /t http://timestamp.digicert.com .\packages\nomacs.x64\data\nomacs-x64\nomacs.exe
+
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /f C:\VSProjects\TU-code-signing.p12  /t http://timestamp.digicert.com .\packages\nomacs.x86\data\nomacs-x86\nomacs.exe
+
 REM update repo
 C:\Qt\QtIFW2.0.1\bin\repogen.exe -p ./packages --update repository
 
@@ -9,5 +14,7 @@ REM C:\Qt\QtIFW2.0.1\bin\binarycreator.exe -c config\config.xml -p packages -r c
 
 REM use for final online installer
 C:\Qt\QtIFW2.0.1\bin\binarycreator.exe -n -c config\config.xml -p packages -r config/resources.qrc nomacs-setup
+
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool" sign /f C:\VSProjects\TU-code-signing.p12  /t http://timestamp.digicert.com nomacs-setup.exe
 
 pause
