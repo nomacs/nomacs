e0505e8364ad87ba0005a448ceb56b5f2ace1322
Stefan Fiel <stefan@nomacs.org>
2016-01-22 11:38:58 +0100

    disabled quazip dependency in travis for mac ... it has QT4 dependencies set MAXOSX_RPATH

--- ImageLounge/cmake/MacBuildTarget.cmake
+++ ImageLounge/cmake/MacBuildTarget.cmake
@@ -1,3 +1,18 @@
+#taken from https://cmake.org/Wiki/CMake_RPATH_handling to avoid warning
+# use, i.e. don't skip the full RPATH for the build tree
+SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+
+# when building, don't use the install RPATH already
+# (but later on when installing)
+SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
+
+# the RPATH to be used when installing
+SET(CMAKE_INSTALL_RPATH "")
+
+# don't add the automatically determined parts of the RPATH
+# which point to directories outside the build tree to the install RPATH
+SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE)
+
 add_definitions(-DWITH_PLUGINS)
 
 include(CheckCXXCompilerFlag)
