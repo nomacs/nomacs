a2c9810654bce20cb45d3ce287137ae9fc02675d
targetcode <vignandocode@gmail.com>
2016-03-13 16:46:32 +0530

    Adding Instagram like Filters for GSOC-2016

--- FilterPlugin/CMakeFiles/FilterPlugin_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "FilterPlugin_automoc.cpp"
-  "CMakeFiles/FilterPlugin_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/FilterPlugin_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
