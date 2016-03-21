3c5197601367b8f9ed985189d0deba240b22ee13
diemmarkus <diemmarkus@gmail.com>
2016-03-21 14:36:23 +0100

    with nomacs in file name

--- ImageLounge/src/DkCore/DkUtils.cpp
+++ ImageLounge/src/DkCore/DkUtils.cpp
@@ -411,7 +411,7 @@ void DkUtils::initializeDebug() {
 QString DkUtils::getLogFilePath() {
 	
 	QString logPath = QStandardPaths::writableLocation(QStandardPaths::TempLocation);
-	QString now = QDateTime::currentDateTime().toString("yyyy-MM-dd HH-mm-ss-");
+	QString now = QDateTime::currentDateTime().toString("nomacs-yyyy-MM-dd HH-mm-ss-");
 
 	QFileInfo fileInfo(logPath);
 	fileInfo.setFile(fileInfo.dir(), now + "log.txt");
