1ff2f87a9d910856c7f2511c810e8cfb5fbba065
praveenagrawal <praveen.agrawal0@gmail.com>
2016-03-25 05:42:27 +0530

    Added InstaLikeFilterPlugin The filter is like the xProII filter of instagram

---
+++ InstaLikeFilterPlugin/CMakeFiles/InstaLikeFilterPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "InstaLikeFilterPlugin_automoc.cpp"
+  "qrc_nomacsPlugin.cpp"
+  "CMakeFiles/InstaLikeFilterPlugin.dir/src/instaFilter.cpp.o"
+  "CMakeFiles/InstaLikeFilterPlugin.dir/qrc_nomacsPlugin.cpp.o"
+  "CMakeFiles/InstaLikeFilterPlugin.dir/InstaLikeFilterPlugin_automoc.cpp.o"
+  "/home/praveen/nomacs-new/nomacs/ImageLounge/plugins/libInstaLikeFilterPlugin.pdb"
+  "/home/praveen/nomacs-new/nomacs/ImageLounge/plugins/libInstaLikeFilterPlugin.so"
+  "/home/praveen/nomacs-new/nomacs/ImageLounge/plugins/libInstaLikeFilterPlugin.so..."
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/InstaLikeFilterPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
