be4ebcb736f61a91e53ab2cb6ed158652e5e07da
praveenagrawal <praveen.agrawal0@gmail.com>
2016-03-25 13:13:04 +0530

    Deleting autogenerated files

--- InstaLikeFilterPlugin/CMakeFiles/InstaLikeFilterPlugin_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "InstaLikeFilterPlugin_automoc.cpp"
-  "CMakeFiles/InstaLikeFilterPlugin_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/InstaLikeFilterPlugin_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- InstaLikeFilterPlugin/CMakeFiles/SimplePlugin_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "SimplePlugin_automoc.cpp"
-  "CMakeFiles/SimplePlugin_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/SimplePlugin_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
