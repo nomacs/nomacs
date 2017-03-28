296aeb3ca9095352c41d68a087a81ec020668f38
diemmarkus <diemmarkus@gmail.com>
2017-03-28 14:44:53 +0200

    CMakeUserSkel.cmake cleaned

--- CMakeUserSkel.cmake
+++ CMakeUserSkel.cmake
@@ -1,24 +1,13 @@
 # If you want to use prefix paths with cmake, copy and rename this file to CMakeUser.txt
 # Do not add this file to GIT!
 
-# set your preferred Qt Library path
+# set your preferred paths
 IF (CMAKE_CL_64)
-	SET(CMAKE_PREFIX_PATH "C:/Qt/qt-everywhere-opensource-src-4.8.5-x64-native-gestures/bin/")
-ELSE ()
-	SET(CMAKE_PREFIX_PATH "C:/Qt/qt-everywhere-opensource-src-4.8.5-x86-native-gestures/bin/")
-ENDIF ()
-
-# set your preferred OpenCV Library path
-IF (CMAKE_CL_64)
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/VSProjects/OpenCV/build2012x64")
-ELSE ()
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/VSProjects/OpenCV/build2012x86")
-ENDIF ()
-
-# set your preferred nomacs Library path
-IF (CMAKE_CL_64)
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/VSProjects/nomacs.git/build2012x64")
-ELSE ()
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/VSProjects/nomacs.git/build2012x86")
-ENDIF ()
-
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/Qt/qt-everywhere-opensource-src-5.8.0-x64/qtbase/bin/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/3rd-party/opencv/build2017-x64/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/nomacs/build2017-x64/")
+ELSE()
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/Qt/qt-everywhere-opensource-src-5.8.0-x86/qtbase/bin/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/3rd-party/opencv/build2017-x86/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "D:/coding/nomacs/build2017-x86/")	
+ENDIF()
