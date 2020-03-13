db4b59452a05dfcd3ac046d7217d3f125b38d623
Markus Diem <diemmarkus@gmail.com>
2020-03-13 09:43:34 +0100

    cmake user paths updated

--- CMakeUserPaths.cmake
+++ CMakeUserPaths.cmake
@@ -1,20 +1,13 @@
-# If you want to use prefix paths with cmake, copy and rename this file to CMakeUser.cmake
+# If you want to use prefix paths with cmake, copy and rename this file to CMakeUser.txt
 # Do not add this file to GIT!
 
-# set your preferred OpenCV Library path
+# set your preferred paths
 IF (CMAKE_CL_64)
-	message(STATUS "using x64 architecture...")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/Qt/Qt5.11.1-x64/bin/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/opencv/build2017-x64/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/LibRaw/build2017-x64/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/nomacs/build2017-x64/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/leptonica/build2017-x64/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/tesseract/build2017-x64/")
-	
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/Qt/Qt5.11.1-x64/bin")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/opencv/build2019-x64/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/nomacs/build2019-x64/")
 ELSE()
-	message(STATUS "using x86 architecture...")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/Qt/Qt5.11.1-x86/bin/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/opencv/build2017-x86/")
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/LibRaw/build2017-x86/")	
-	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/nomacs/build2017-x86/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/Qt/qt-everywhere-opensource-src-5.8.0-x86/qtbase/bin/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/3rd-party/opencv/build2019-x86/")
+	SET(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "C:/coding/nomacs/build2019-x86/")	
 ENDIF()
