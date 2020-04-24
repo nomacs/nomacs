cd171b904ac008e2b33e893e63c3aa111ac51459
Markus Diem <diemmarkus@gmail.com>
2020-04-24 21:44:24 +0200

    add cmake_prefix_path for nomacs
    - fix path with \ in make-exiv.bat

--- 3rd-party/make-exiv2.bat
+++ 3rd-party/make-exiv2.bat
@@ -1,15 +1,18 @@
 REM Build exiv2 - be sure to run build-expat.bat and build-opencv.bat first
 
 SET MYPATH=%~dp0
+REM fix windows backslashed paths : /
+SET MYPATH=%MYPATH:\=/%
 SET NAME=exiv2
 SET BUILD_DIR=%mypath%/%name%/build
 
 REM build exiv2
-cmake -DEXPAT_BUILD_PATH="%mypath%/expat/build"^
-    -DEXPAT_INCLUDE_DIR="C:/coding/3rd-party/expat/lib"^
-    -DZLIB_INCLUDE_DIR="C:/coding/3rd-party/opencv/3rdparty/zlib"^
-    -DZLIB_BUILD_PATH="%mypath%/opencv/build/3rdparty"^
-    -B%build_dir% %mypath%/%name%
+cmake^
+ -DEXPAT_BUILD_PATH="%mypath%/expat/build"^
+ -DEXPAT_INCLUDE_DIR="C:/coding/3rd-party/expat/lib"^
+ -DZLIB_INCLUDE_DIR="C:/coding/3rd-party/opencv/3rdparty/zlib"^
+ -DZLIB_BUILD_PATH="%mypath%/opencv/build/3rdparty"^
+ -B%build_dir% %mypath%/%name%
 
 msbuild -property:Configuration=Debug  %build_dir%/%name%.sln
 msbuild -property:Configuration=Release  %build_dir%/%name%.sln
