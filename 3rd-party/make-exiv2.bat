52093d3dba3e65b0b9e5aafa2537cab806e8941f
Markus Diem <diemmarkus@gmail.com>
2020-04-24 21:19:45 +0200

    exiv2 lib paths corrected

--- 3rd-party/make-exiv2.bat
+++ 3rd-party/make-exiv2.bat
@@ -5,7 +5,11 @@ SET NAME=exiv2
 SET BUILD_DIR=%mypath%/%name%/build
 
 REM build exiv2
-cmake -DEXPAT_BUILD_PATH="%mypath%/expat/build" -DZLIB_BUILD_PATH="%mypath%/opencv/build/3rdparty" -B%build_dir% %mypath%/%name%
+cmake -DEXPAT_BUILD_PATH="%mypath%/expat/build"^
+    -DEXPAT_INCLUDE_DIR="C:/coding/3rd-party/expat/lib"^
+    -DZLIB_INCLUDE_DIR="C:/coding/3rd-party/opencv/3rdparty/zlib"^
+    -DZLIB_BUILD_PATH="%mypath%/opencv/build/3rdparty"^
+    -B%build_dir% %mypath%/%name%
 
 msbuild -property:Configuration=Debug  %build_dir%/%name%.sln
 msbuild -property:Configuration=Release  %build_dir%/%name%.sln
--- 3rd-party/make.bat
+++ 3rd-party/make.bat
@@ -1,8 +1,10 @@
 REM Be sure to run this in a VS Native Tools environment
 REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
+SET QT_PATH="C:/Qt/Qt-5.14.1-installer/5.14.2/msvc2017_64/bin"
 SET MYPATH=%~dp0
 
+REM call %mypath%/make-quazip.bat %qt_path%
 call %mypath%/make-opencv.bat
 call %mypath%/make-expat.bat
 call %mypath%/make-exiv2.bat
