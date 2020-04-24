fcc932016eba62ba45465bb1ce1426a6b4babe4f
Markus Diem <diemmarkus@gmail.com>
2020-04-24 16:03:51 +0200

    file rename reflected in master make.bat

--- 3rd-party/make.bat
+++ 3rd-party/make.bat
@@ -1,6 +1,6 @@
 REM Be sure to run this in a VS Native Tools environment
 REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
-call build-opencv.bat
-call build-expat.bat
-call build-exiv2.bat
+call make-opencv.bat
+call make-expat.bat
+call make-exiv2.bat
