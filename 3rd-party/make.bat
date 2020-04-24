e1d67142b279cd83ae89c277273027c6e3c8ff3b
Markus Diem <diemmarkus@gmail.com>
2020-04-24 22:42:53 +0200

    make.bat: qt_path as argument

--- 3rd-party/make.bat
+++ 3rd-party/make.bat
@@ -1,7 +1,11 @@
 REM Be sure to run this in a VS Native Tools environment
 REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
-SET QT_PATH="C:/Qt/Qt-5.14.1-installer/5.14.2/msvc2017_64/bin"
+REM SET QT_PATH="C:/Qt/Qt-5.14.1-installer/5.14.2/msvc2017_64/bin"
+SET QT_PATH=%1
+REM fix windows backslashed paths : /
+SET QT_PATH=%QT_PATH:\=/%
+
 SET MYPATH=%~dp0
 
 call %mypath%/make-quazip.bat %qt_path%
