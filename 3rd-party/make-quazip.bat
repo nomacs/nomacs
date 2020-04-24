578bf1da201c51e37167dacaa56695ea977675cb
Markus Diem <diemmarkus@gmail.com>
2020-04-24 22:36:12 +0200

    adds submodule quazip

---
+++ 3rd-party/make-quazip.bat
@@ -0,0 +1,19 @@
+REM build quazip
+
+SET MYPATH=%~dp0
+REM fix windows backslashed paths : /
+SET MYPATH=%MYPATH:\=/%
+
+SET QT_PATH=%1
+SET NAME=quazip
+SET BUILD_DIR=%mypath%/%name%/build
+
+echo %qt_path%
+
+cmake -DCMAKE_PREFIX_PATH=%qt_path%^
+ -DZLIB_INCLUDE_DIRS="%mypath%opencv/3rdparty/zlib"^
+ -DZLIB_BUILD_PATH="%mypath%opencv/build/3rdparty"^
+ -B%build_dir% %mypath%/%name%
+
+msbuild -target:quazip5 -property:Configuration=Debug  %build_dir%/%name%.sln
+msbuild -target:quazip5 -property:Configuration=Release  %build_dir%/%name%.sln
--- 3rd-party/make.bat
+++ 3rd-party/make.bat
@@ -4,7 +4,7 @@ REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxilia
 SET QT_PATH="C:/Qt/Qt-5.14.1-installer/5.14.2/msvc2017_64/bin"
 SET MYPATH=%~dp0
 
-REM call %mypath%/make-quazip.bat %qt_path%
+call %mypath%/make-quazip.bat %qt_path%
 call %mypath%/make-opencv.bat
 call %mypath%/make-expat.bat
 call %mypath%/make-exiv2.bat
