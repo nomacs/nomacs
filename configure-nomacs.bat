43ba8ec59f9a9a9aa6f38ac3410098511a3981e5
Markus Diem <diemmarkus@gmail.com>
2020-04-28 14:55:12 +0200

    fixes Qt path (they seem to have updated it)

--- configure-nomacs.bat
+++ configure-nomacs.bat
@@ -1,19 +1,25 @@
-REM configure nomacss
+REM configure nomacs
 
 SET MYPATH=%~dp0
+SET QT5=%1
+
 REM fix windows backslashed paths : /
 SET MYPATH=%MYPATH:\=/%
+SET QT5=%QT5:\=/%
 
-SET QT5=%1
 SET NAME=nomacs
-SET BUILD_DIR=%mypath%/build/%name%
+SET BUILD_DIR=%mypath%build/%name%
+
+echo %QT5%
 
 cmake^
  -DENABLE_TRANSLATIONS=ON^
- -DCMAKE_PREFIX_PATH="%QT5%;%mypath%3rd-party/build/exiv2;%mypath%3rd-party/build/opencv;%mypath%3rd-party/build/quazip"^
+ -DCMAKE_PREFIX_PATH="%QT5%;%mypath%3rd-party/build/exiv2;%mypath%3rd-party/build/opencv;%mypath%3rd-party/build/quazip;%mypath%3rd-party/build/libraw;"^
  -DENABLE_RAW=OFF^
  -DENABLE_QUAZIP=OFF^
- -B%build_dir% %mypath%/ImageLounge
+  -B%build_dir% %mypath%/ImageLounge
+
+REM cmake --build %build_dir% -- -m
 
 REM msbuild -m -verbosity:minimal -property:Configuration=Debug  %build_dir%/%name%.sln
 REM msbuild -m -verbosity:minimal -property:Configuration=Release  %build_dir%/%name%.sln
