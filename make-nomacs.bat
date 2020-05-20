93588a801c87212650fecee2715cbf7717e87998
Markus Diem <diemmarkus@gmail.com>
2020-05-20 09:00:22 +0200

    cmake: translation is now turned on

--- make-nomacs.bat
+++ make-nomacs.bat
@@ -1,7 +1,7 @@
+@echo off
 REM configure nomacs
 
 SET MYPATH=%~dp0
-SET QT5=%1
 
 REM fix windows backslashed paths : /
 SET MYPATH=%MYPATH:\=/%
@@ -10,18 +10,12 @@ SET QT5=%QT5:\=/%
 SET NAME=nomacs
 SET BUILD_DIR=%mypath%build/%name%
 
-echo %QT5%
+echo "Qt Path:" %QT5%
 
 cmake^
- -DENABLE_TRANSLATIONS=OFF^
- -DCMAKE_PREFIX_PATH="%QT5%;%mypath%3rd-party/build/exiv2;%mypath%3rd-party/build/opencv;%mypath%3rd-party/build/quazip;%mypath%3rd-party/build/libraw;"^
+ -DCMAKE_PREFIX_PATH="%QT5%;%mypath%3rd-party/build/exiv2;%mypath%3rd-party/build/opencv;%mypath%3rd-party/build/quazip;%mypath%3rd-party/build/libraw;%mypath%3rd-party/build/imageformats/libheif;%mypath%3rd-party/build/imageformats/libde265"^
+ -DENABLE_TRANSLATIONS=ON^
  -B%build_dir% %mypath%/ImageLounge
 
 cmake --build %build_dir% --config Release -- -m
 cmake --build %build_dir% --config Release --target INSTALL -- -m
-
-
-REM cmake --build %build_dir% -- -m
-
-REM msbuild -m -verbosity:minimal -property:Configuration=Debug  %build_dir%/%name%.sln
-REM msbuild -m -verbosity:minimal -property:Configuration=Release  %build_dir%/%name%.sln
