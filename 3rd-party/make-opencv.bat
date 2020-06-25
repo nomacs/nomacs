f70e6b96378f88b13cdf7e7eee21552f671f9756
Markus Diem <diemmarkus@gmail.com>
2020-06-25 15:08:59 +0200

    make opencv: minimal build

--- 3rd-party/make-opencv.bat
+++ 3rd-party/make-opencv.bat
@@ -13,7 +13,25 @@ if exist %build_dir%/bin/Release/ (
 
 REM build opencv (that could take a while...)
 echo building %name%
-cmake -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -B%build_dir% %mypath%/%name%
+
+cmake^
+ -DBUILD_PERF_TESTS=OFF^
+ -DBUILD_TESTS=OFF^
+ -DBUILD_opencv_java=OFF^
+ -DBUILD_opencv_java_bindings_generator=OFF^
+ -DBUILD_opencv_python=OFF^
+ -DBUILD_opencv_apps=OFF^
+ -DBUILD_opencv_dnn=OFF^
+ -DBUILD_opencv_calib3d=OFF^
+ -DBUILD_opencv_highgui=OFF^
+ -DBUILD_opencv_photo=OFF^
+ -DBUILD_opencv_python3=OFF^
+ -DBUILD_opencv_python_tests=OFF^
+ -DBUILD_opencv_python_bindings_generator=OFF^
+ -DBUILD_opencv_stitiching=OFF^
+ -DBUILD_opencv_video=OFF^
+ -DBUILD_opencv_videoio=OFF^
+ -B%build_dir% %mypath%/%name%
 
 cmake --build %build_dir% --config Release -- -m
 
