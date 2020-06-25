796bbd56f48e0b74a5805a2e5e642fb1460c3280
Markus Diem <diemmarkus@gmail.com>
2020-06-25 11:45:51 +0200

    make scripts: add external dependencies

--- make-nomacs.bat
+++ make-nomacs.bat
@@ -1,26 +1,38 @@
 @echo off
-REM configure nomacs
-REM "C:/Qt/Qt-5.14.1-installer/5.14.2/msvc2017_64/bin"
+REM build nomacs
+REM make-nomacs "C:/Qt/Qt-5.14.1-installer/5.14.2/msvc2017_64/bin" debug "C:/coding/nomacs/nomacs/3rd-party/build"
+REM note to myself: it is already complex enough that we should script it in python
 
-SET MYPATH=%~dp0
-SET QT5=%1
-SET RELEASE="%2"
+set my_path=%~dp0
+set qt5=%1
+set release="%2"
+set dependency_path=%3
+
+if NOT "%3" == "" (
+    goto :custom_3rdparty_path
+) 
+
+REM default to repo build
+set dependency_path=%my_path%3rd-party/build
+
+:custom_3rdparty_path
 
 REM fix windows backslashed paths : /
-SET MYPATH=%MYPATH:\=/%
-SET QT5=%QT5:\=/%
+set my_path=%my_path:\=/%
+set qt5=%qt5:\=/%
 
-SET NAME=nomacs
-SET BUILD_DIR=%mypath%build/%name%
+set name=nomacs
+set build_dir=%my_path%build/%name%
 
-echo "Qt Path:" %QT5%
+echo Qt path: %qt5%
 
 cmake^
- -DCMAKE_PREFIX_PATH="%QT5%;%mypath%3rd-party/build/exiv2;%mypath%3rd-party/build/opencv;%mypath%3rd-party/build/quazip;%mypath%3rd-party/build/libraw;%mypath%3rd-party/build/imageformats/libheif;%mypath%3rd-party/build/imageformats/libde265"^
+ -DCMAKE_PREFIX_PATH="%qt5%;"^
+ -DDEPENDENCY_PATH=%dependency_path%^
  -DENABLE_TRANSLATIONS=ON^
  -DENABLE_HEIF=ON^
  -DENABLE_AVIF=ON^
- -B%build_dir% %mypath%/ImageLounge
+ -B%build_dir% %my_path%/ImageLounge
 
 cmake --build %build_dir% --config Release -- -m
 
