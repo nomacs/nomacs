addd1b18d923924fd130b6017d95edaf33b5eae8
Markus Diem <diemmarkus@gmail.com>
2020-06-25 13:38:57 +0200

    version incrementer: manipulates setup files too

--- ImageLounge/version-incrementer.py
+++ ImageLounge/version-incrementer.py
@@ -19,7 +19,7 @@ __status__ = "Production"
 
 OUTPUT_NAME = "version-incrementer"
 
-def increment(filepath: str):
+def increment(v: str, filepath: str):
     from shutil import move
     from os import remove
 
@@ -31,60 +31,69 @@ def increment(filepath: str):
         
             for l in src.readlines():
 
-                l = increment_version_string(l)
-                l = increment_version(l)
+                l = increment_version_string(v, l)
+                l = increment_version(v, l)
 
-                l = add_git_tag_string(l)
+                l = add_git_tag_string(v, l)
 
                 dst.write(l)    
 
     remove(filepath)
     move(outpath, filepath)
 
-def increment_version_string(line: str):
 
-    # seraching: #define NOMACS_VERSION_STR "0.3.5.0\0"
-    if "NOMACS_VERSION_STR" in line:
+def increment_version_string(new_v: str, line: str):
+
+    # searching (DkVersion.h): #define NOMACS_VERSION_STR "0.3.5.0\0"
+    # searching (msi installer): <?define ProductVersion = "3.14.42"?>
+    # searching (inno installer): define MyAppVersion "3.14.5"
+    if "NOMACS_VERSION_STR" in line or \
+        "<?define ProductVersion" in line or \
+        "define MyAppVersion" in line:
         
-        line = line.replace("\n", "")
-        v = line.split(".")
+        str_ver = line.split("\"")
+        old_v = str_ver[1].split(".")
 
-        build = v[-1].split("\"")
-
-        if len(v) == 4:
+        if len(old_v) == 4:
             # increment
-            build[0] = str(int(build[0])+1)
-            v[-1] = "\"".join(build)
-        elif len(v) == 3:
-            v[-1] = build[0] + ".0" + build[1]
-        
+            build = str(int(old_v[-1])+1)
+        elif len(old_v) == 3:
+            build = "0"
 
-        line = ".".join(v) + "\n"
+        line = str_ver[0] + "\"" + new_v + "." + build + "\"" + str_ver[-1]
 
-        print("[Version Incrementer] version updated: " + line.split("\"")[-2])
+        # send status message only once
+        if "NOMACS_VERSION_STR" in line:
+            print("[Version Incrementer] version updated: " + line.split("\"")[-2])
 
     return line
 
-def increment_version(line: str):
+def increment_version(new_v: str, line: str):
     
-    # seraching: #define NOMACS_VERSION_RC 0,3,5,0
+    # searching: #define NOMACS_VERSION_RC 0,3,5,0
     if "NOMACS_VERSION_RC" in line:
 
-        line = line.replace("\n", "")
-        v = line.split(",")
+        str_ver = line.split(" ")
+        v = str_ver[-1].split(",")
 
         if len(v) == 4:
-            v[-1] = str(int(v[-1])+1)
+            build = str(int(v[-1])+1)
         elif len(v) == 3:
-            v[-1] += ",0"
+            build += ",0"
+        else:
+            print("cannot increment: " + line)
+            return line
 
-        line = ",".join(v) + "\n"
+        new_v = new_v.replace(".", ",")
+        str_ver[-1] = new_v + "," + build
+ 
+        line = " ".join(str_ver) + "\n"
 
     return line
 
-def add_git_tag_string(line: str):
+def add_git_tag_string(version: str, line: str):
 
-    # seraching: #define NOMACS_GIT_TAG "4add4f1f6b6c731a9f4cf63596e087d4f68c2aed"
+    # searching: #define NOMACS_GIT_TAG "4add4f1f6b6c731a9f4cf63596e087d4f68c2aed"
     if "NOMACS_GIT_TAG" in line:
 
         line = line.replace("\n", "")
@@ -113,8 +122,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
        description='Increments the build version of a C++ project and adds the git rev as product version.')
 
+    parser.add_argument("version", type=str,
+                        help="""current version""")
+
     parser.add_argument("inputfile", type=str,
-                        help="""full path to the project's DkVersion.h file""")
+                        help="""full path to the file""")
+
 
     args = parser.parse_args()
 
@@ -122,4 +135,4 @@ if __name__ == "__main__":
         print("input file does not exist: " + args.inputfile)
         exit()
 
-    increment(args.inputfile)
+    increment(args.version, args.inputfile)
