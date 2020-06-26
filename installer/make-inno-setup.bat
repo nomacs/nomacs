d207fe4476adf15b15a0b872885e0878c7f304ad
Markus Diem <diemmarkus@gmail.com>
2020-06-26 09:45:58 +0200

    appveyor: inno setup path

--- installer/make-inno-setup.bat
+++ installer/make-inno-setup.bat
@@ -1,6 +1,11 @@
 echo off
+
+set inno_local = "C:\Users\markus\AppData\Local\Programs\Inno Setup 6"
+set inno_appveyor = "C:\Users\markus\AppData\Local\Programs\Inno Setup 6"
+set sign = "C:\Program Files (x86)\Windows Kits\10\bin\x64\"
+
 REM set your inno setup path
-SET PATH=%PATH%;"C:\Users\markus\AppData\Local\Programs\Inno Setup 6";"C:\Program Files (x86)\Windows Kits\10\bin\x64\";
+SET PATH=%PATH%;%inno_local%;%inno_appveyor%;%sign%;
 SET ARCH=x64
 
 call sign ./nomacs.%ARCH%/nomacs.exe %~1
