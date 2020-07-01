a407cc7b8f3af30148c9c3b9b009be4c20bc0816
Markus Diem <diemmarkus@gmail.com>
2020-07-01 09:59:57 +0200

    changelog updated

--- ImageLounge/src/DkCore/DkVersion.h
+++ ImageLounge/src/DkCore/DkVersion.h
@@ -3,6 +3,6 @@
 // In addition to the version number displayed in the help section, 
 // it will update the VS_VERSION_INFO in the *.rc file.
 
-#define NOMACS_VERSION_RC 3,15,0,1416
-#define NOMACS_VERSION_STR "3.15.0.1416"
-#define NOMACS_GIT_TAG "ff0df98870a363272cbf070b093c75e4a5a0e4d8"
+#define NOMACS_VERSION_RC 3,15,0,1418
+#define NOMACS_VERSION_STR "3.15.0.1418"
+#define NOMACS_GIT_TAG "8b35138f0209bcc7655b2dbcf6e1770d0d66d1d7"
