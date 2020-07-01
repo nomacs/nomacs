b8f3d780a23a9b137f77f6a87616e8a51f2bc3ae
Markus Diem <diemmarkus@gmail.com>
2020-07-01 15:59:51 +0200

    fixes zlib include

--- ImageLounge/src/DkCore/DkVersion.h
+++ ImageLounge/src/DkCore/DkVersion.h
@@ -3,6 +3,6 @@
 // In addition to the version number displayed in the help section, 
 // it will update the VS_VERSION_INFO in the *.rc file.
 
-#define NOMACS_VERSION_RC 3,15,0,1418
-#define NOMACS_VERSION_STR "3.15.0.1418"
-#define NOMACS_GIT_TAG "8b35138f0209bcc7655b2dbcf6e1770d0d66d1d7"
+#define NOMACS_VERSION_RC 3,15,0,1424
+#define NOMACS_VERSION_STR "3.15.0.1424"
+#define NOMACS_GIT_TAG "7977badbc9ccd1be4fa1d86037482b0d25dfac46"
