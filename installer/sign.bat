cbb3eb47bc5037eab5cfd79ff7eb970f0648fce4
Markus Diem <diemmarkus@gmail.com>
2020-08-13 08:43:59 +0200

    appveyor: do not sign PRs

--- installer/sign.bat
+++ installer/sign.bat
@@ -1,29 +1,21 @@
-echo off
 REM this script signs the exe provided, use:
 REM sign nomacs.x64/nomacs.exe cert-password
 
 REM the second argument is assumed to be the certificate's password (for appveyor)
-if "%~2"=="" (
-    goto LocalCert
-) else (
-    goto RepoCert
-)
-
-:LocalCert
-
-REM set code page to utf-8 (for german umlaute)
-chcp 65001
-
-REM sign using the installed certificate
-signtool sign /n "Technische Universität Wien" /t http://timestamp.digicert.com %~1
-
-goto End
-
-:RepoCert
+if "%~2" == "" goto local
 
 REM sign with the repositorie's certificate (password has to be provided)
+
+echo online signing...
+echo arg: %2
 signtool sign /t http://timestamp.digicert.com /f ./cert-2018.p12 /p %2 /a %~1
+goto end
 
-goto End
+REM REM set code page to utf-8 (for german umlaute)
+REM :local
+REM chcp 65001
 
-:End
+REM REM sign using the installed certificate
+REM signtool sign /n "Technische Universität Wien" /t http://timestamp.digicert.com %~1
+
+:end
