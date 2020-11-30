6ef605cbefaec73e87f8259c4383a52b9bc8c541
Daniel Novomesky <dnovomesky@gmail.com>
2020-11-30 17:03:49 +0100

    Avoid downgrading AVIF images to 8bit during save.

--- ImageLounge/src/DkCore/DkBasicLoader.cpp
+++ ImageLounge/src/DkCore/DkBasicLoader.cpp
@@ -1131,7 +1131,7 @@ bool DkBasicLoader::saveToBuffer(const QString& filePath, const QImage& img, QSh
 
 #if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
 		// JPEG 2000 can only handle 32 or 8bit images
-		if (!hasAlpha && img.colorTable().empty() && !fInfo.suffix().contains(QRegExp("(j2k|jp2|jpf|jpx|png)"))) {
+		if (!hasAlpha && img.colorTable().empty() && !fInfo.suffix().contains(QRegExp("(avif|j2k|jp2|jpf|jpx|png)"))) {
 			sImg = sImg.convertToFormat(QImage::Format_RGB888);
 		}
 		else if (fInfo.suffix().contains(QRegExp("(j2k|jp2|jpf|jpx)")) && sImg.depth() != 32 && sImg.depth() != 8) {
