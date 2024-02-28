5b0dd9716352c846d5193b0b142f45cec747ea59
Tzu-Yu Lee <leejuyuu@gmail.com>
2024-02-28 20:28:04 +0800

    Update Git revision tagging
    Building Nomacs from the zip source archives
    downloaded from Github fails because the `versionupdate.py` script
    calls Git command, which assumes the working direction is a child of
    a Git repo.
    
    Move the revision tag logic to the CMake script. Check for the existence
    of a `.git` directory to fix the issue.
    
    Also, include the commit date and check whether there are modified files
    in the repository. These could be useful when reporting and searching
    for bugs. Assemble the result into a constant string for use.
    
    Fixes https://github.com/nomacs/nomacs/issues/1034.

--- scripts/versionupdate.py
+++ scripts/versionupdate.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 """Increments the build version of a C++ projects.
 
-This script increments the build version after every build of 
+This script increments the build version after every build of
 a Visual Stuido project. It manipulates the DkVersion.h file in order
 to do so. The version number defined there is displayed in help and
 used for the VS_VERSION_INFO in the *.rc file
 """
-import logging
 
 __author__ = "Markus Diem"
 __credits__ = ["Markus Diem"]
@@ -20,7 +19,7 @@ __status__ = "Production"
 OUTPUT_NAME = "versionupdate"
 
 
-def update(filepath, copy = False):
+def update(filepath, copy=False):
     from shutil import move
     from os import remove
     from utils.fun import version
@@ -32,16 +31,12 @@ def update(filepath, copy = False):
 
     with open(filepath, "r") as src:
         with open(dstpath, "w") as dst:
-        
             for l in src.readlines():
-
                 l = update_version_string(v, l)
                 l = update_version_rc(v, l)
                 l = update_version_patch(v, l)
 
-                l = add_git_tag_string(l)
-
-                dst.write(l)    
+                dst.write(l)
 
     # replace current file?
     if not copy:
@@ -50,33 +45,32 @@ def update(filepath, copy = False):
 
 
 def update_version_string(version, line):
-
     # searching (DkVersion.h): #define NOMACS_VERSION_STR "3.14.42\0"
     # searching (msi installer): <?define ProductVersion = "3.14.42"?>
     # searching (inno installer): define MyAppVersion "3.14.42"
-    if "NOMACS_VERSION_STR" in line or \
-        "<?define ProductVersion" in line or \
-        "define MyAppVersion" in line:
-        
-        str_ver = line.split("\"")
-        line = str_ver[0] + "\"" + version + "\"" + str_ver[-1]
+    if (
+        "NOMACS_VERSION_STR" in line
+        or "<?define ProductVersion" in line
+        or "define MyAppVersion" in line
+    ):
+        str_ver = line.split('"')
+        line = str_ver[0] + '"' + version + '"' + str_ver[-1]
 
     return line
 
+
 def update_version_rc(version, line):
-    
     # searching: #define NOMACS_VERSION_RC 3,14,42
     if "NOMACS_VERSION_RC" in line:
-
         str_ver = line.split(" ")
         str_ver[-1] = version.replace(".", ",")
- 
+
         line = " ".join(str_ver) + "\n"
 
     return line
 
-def update_version_patch(version, line):
 
+def update_version_patch(version, line):
     # get patch from 3.14.42
     vs = version.split(".")
 
@@ -86,7 +80,6 @@ def update_version_patch(version, line):
 
     # searching: #define NOMACS_VER_PATCH 0
     if "NOMACS_VER_PATCH" in line:
-
         str_ver = line.split(" ")
         str_ver[-1] = vs[-1]
 
@@ -94,43 +87,23 @@ def update_version_patch(version, line):
 
     return line
 
-def add_git_tag_string(line):
-
-    # searching: #define NOMACS_GIT_TAG "4add4f1f6b6c731a9f4cf63596e087d4f68c2aed"
-    if "NOMACS_GIT_TAG" in line:
-
-        line = line.replace("\n", "")
-
-        v = line.split("\"")
-
-        if len(v) == 3:
-            v[-2] = git_tag()
-
-        line = "\"".join(v) + "\n"
-
-    return line
-
-def git_tag():
-    import subprocess
-
-    tag = subprocess.check_output(["git", "rev-parse", "HEAD"])
-    tag = tag.strip().decode("utf-8")
-
-    return tag
 
 if __name__ == "__main__":
     import argparse
     import os
-    from utils.fun import mypath
 
     parser = argparse.ArgumentParser(
-       description='Increments the build version of a C++ project and adds the git rev as product version.')
+        description="Increments the build version of a C++ project."
+    )
 
-
-    parser.add_argument("inputfile", type=str,
-                        help="""full path to the file who's version should be updated""")
-    parser.add_argument("--copy", action='store_true',
-                        help="""if set, a _ver file will be created""")
+    parser.add_argument(
+        "inputfile",
+        type=str,
+        help="""full path to the file who's version should be updated""",
+    )
+    parser.add_argument(
+        "--copy", action="store_true", help="""if set, a _ver file will be created"""
+    )
 
     args = parser.parse_args()
 
