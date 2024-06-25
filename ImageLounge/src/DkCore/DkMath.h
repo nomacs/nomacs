da7734820dd56eac46ceb978e26708db76652833
Daniel Novomeský <dnovomesky@gmail.com>
2024-06-25 21:30:43 +0200

    Delete problematic character from comment
    Fixes following warning:
    warning C4828: The file contains a character starting at offset
    0x4f3a that is illegal in the current source character set
    (codepage 65001).

--- ImageLounge/src/DkCore/DkMath.h
+++ ImageLounge/src/DkCore/DkMath.h
@@ -749,7 +749,7 @@ public:
     /**
      * Normal vector.
      * @return a vector which is normal to the current vector
-     * (rotated by 90° counter clockwise).
+     * (rotated by 90 counter clockwise).
      */
     DkVector normalVec() const
     {
