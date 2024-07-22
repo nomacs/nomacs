ad30c6519d3b9796817107a86617270a70725021
Scrubs <scrubbbbs@gmail.com>
2024-07-22 07:20:36 -0500

    Frameless: fix bugs and improve mode switching
    PR #1128 introduced a regression that broke frameless mode on Windows.
    The root cause seems to be a Qt issue with saveGeometry()/
    restoreGeometry() which also saves/restores the windowState()
    (fullscreen,maximized etc) in addition to the geometry.
    
    This patch drops saveGeometry()/restoreGeometry() to fix these bugs and
    to get more control over our window state. It also drops the other
    workarounds which are no longer needed.
    
    Fixes #1134
    Fixes #1032
    Fixes #765
    Fixes #781
    
    Improves upon PR #1128, #1122
    
    Functional changes:
    - save appMode at exit, and use it to decide if we are fullscreen at
      startup
    - -f/--fullscreen switch is no longer temporary, similar to -m/--mode
    - do not use saveGeometry()/restoreGeometry(), it is bugged when
      switching between the different modes
    - offscreen/occluded saved window geometry is corrected by ourselves
      rather than by Qt and/or the window manager.
    - use showFullscreen()/showMaximized()/showNormal(), at startup
    - don't use setWindowState() anymore, bugged on maximized windows
    - make default window size slightly bigger to fit the settings tab
    
    Refactoring:
    - add helpers to convert between normal vs fullscreen modes
    - remove fullscreen switch from DkViewPort since it is always a
      child widget of main window, this has no effect
    - remove an old hack (pre-github) to restore window geometry; does not
      seem to be needed anymore
    - WIP: make currentAppMode obsolete, and use appMode instead
    - use settings manager in main(), load mode setting later
    - add more logging for mode switching and startup sequence
    - chooseMonitor() now compatible with Qt6

--- ImageLounge/src/DkGui/DkNoMacs.h
+++ ImageLounge/src/DkGui/DkNoMacs.h
@@ -129,8 +129,6 @@ public:
 
     void loadFile(const QString &filePath);
 
-    bool mSaveSettings = true;
-
 signals:
     void sendArrangeSignal(bool overlaid) const;
     void closeSignal() const;
@@ -232,6 +230,7 @@ protected:
     bool mOtherKeyPressed = true;
     QPoint mPosGrabKey;
     bool mOverlaid = false;
+    bool mWasMaximized = false;
 
     // menu
     DkMenuBar *mMenu = 0;
@@ -330,16 +329,11 @@ public:
     virtual ~DkNoMacsFrameless();
 
 public slots:
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    void chooseMonitor(bool force = true);
-#endif
+    void chooseMonitor(bool force);
 
 protected:
-    void closeEvent(QCloseEvent *event) override;
     bool eventFilter(QObject *obj, QEvent *event) override;
     virtual void createContextMenu() override;
-
-    QDesktopWidget *mDesktop = 0;
 };
 
 class DllCoreExport DkNoMacsContrast : public DkNoMacsSync
