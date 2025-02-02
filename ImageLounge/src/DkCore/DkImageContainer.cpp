eba2a5d9a0c6ba363ec4f0cbeb87e75cd7ec0118
Tzu-Yu Lee <leejuyuu@gmail.com>
2025-02-02 15:06:52 +0800

    refactor: remove unused mThumb in DkImageContainer
    Remove the unused mThumb and its getter and setters from
    DkImageContainer.

--- ImageLounge/src/DkCore/DkImageContainer.cpp
+++ ImageLounge/src/DkCore/DkImageContainer.cpp
@@ -201,15 +201,6 @@ QSharedPointer<DkMetaDataT> DkImageContainer::getMetaData()
     return getLoader()->getMetaData();
 }
 
-QSharedPointer<DkThumbNailT> DkImageContainer::getThumb()
-{
-    if (!mThumb) {
-        mThumb = QSharedPointer<DkThumbNailT>(createThumb().release());
-    }
-
-    return mThumb;
-}
-
 std::unique_ptr<DkThumbNailT> DkImageContainer::createThumb()
 {
 #ifdef WITH_QUAZIP
@@ -230,7 +221,6 @@ QSharedPointer<DkImageContainerT> DkImageContainerT::fromImageContainer(QSharedP
     imgCT->mLoader = imgC->getLoader();
     imgCT->mEdited = imgC->isEdited();
     imgCT->mSelected = imgC->isSelected();
-    imgCT->mThumb = imgC->getThumb();
     imgCT->mLoadState = imgC->getLoadState();
     imgCT->mFileBuffer = imgC->getFileBuffer();
 
@@ -647,7 +637,6 @@ bool DkImageContainerT::loadImageThreaded(bool force)
 
     if (force || fileInfo.lastModified() != modifiedBefore || getLoader()->isDirty()) {
         qDebug() << "updating image...";
-        getThumb()->setImage(QImage());
         clear();
     }
 
@@ -788,8 +777,6 @@ void DkImageContainerT::loadingFinished()
         emit fileLoadedSignal(false);
         mLoadState = exists_not;
         return;
-    } else if (!getThumb()->hasImage()) {
-        getThumb()->setImage(getLoader()->image());
     }
 
     // clear file buffer if it exceeds a certain size?! e.g. psd files
@@ -989,16 +976,6 @@ QSharedPointer<DkBasicLoader> DkImageContainerT::getLoader()
     }
 
     return mLoader;
-}
-
-QSharedPointer<DkThumbNailT> DkImageContainerT::getThumb()
-{
-    if (!mThumb) {
-        DkImageContainer::getThumb();
-        connect(mThumb.data(), &DkThumbNailT::thumbLoadedSignal, this, &DkImageContainerT::thumbLoadedSignal);
-    }
-
-    return mThumb;
 }
 
 bool DkImageContainerT::isFileDownloaded() const
