5c93c175b2f029795c199334711c058c01a97228
Scrubs <scrubbbbs@gmail.com>
2025-03-24 18:46:23 -0500

    fix: disable animation in manipulator controls
    These were implicilty disabled due to masking of QWidget::setVisible in
    the previous version of FadeWidget

--- ImageLounge/src/DkGui/DkManipulatorWidgets.cpp
+++ ImageLounge/src/DkGui/DkManipulatorWidgets.cpp
@@ -210,9 +210,10 @@ void DkEditDock::setImage(QSharedPointer<DkImageContainerT> imgC)
 
 // DkManipulatorWidget --------------------------------------------------------------------
 DkBaseManipulatorWidget::DkBaseManipulatorWidget(QSharedPointer<DkBaseManipulatorExt> manipulator, QWidget *parent)
-    : DkFadeWidget(parent)
+    : DkWidget(parent)
 {
     mBaseManipulator = manipulator;
+    setVisible(false);
 }
 
 QSharedPointer<DkBaseManipulatorExt> DkBaseManipulatorWidget::baseManipulator() const
