468ff879405247c9a923f74302bce9a1d05da866
Scrubs <scrubbbbs@gmail.com>
2025-03-26 15:34:52 -0500

    fix: null pointer in DkBatch profiles
    only possible if plugins are disabled

--- ImageLounge/src/DkGui/DkBatch.cpp
+++ ImageLounge/src/DkGui/DkBatch.cpp
@@ -3006,8 +3006,11 @@ void DkBatchWidget::loadProfile(const QString &profilePath)
 
 void DkBatchWidget::applyDefault()
 {
-    for (DkBatchContainer *bc : mWidgets)
-        bc->batchContent()->applyDefault();
+    for (DkBatchContainer *w : mWidgets) {
+        if (!w)
+            continue;
+        w->batchContent()->applyDefault();
+    }
 }
 
 void DkBatchWidget::widgetChanged()
