1fc8fb461c14251ac37e44967a7021af25e5635e
Scrubs <scrubbbbs@gmail.com>
2025-03-27 15:53:22 -0500

    cmake: add precompiled header (default disabled)
    This is around 25% faster (at best) for clang-19/debug/qt6 target
    
    For individual files this particular set of headers is only a few
    seconds faster, but at least not slower.  It mainly helps when modifying
    a common header like DkSettings.
    
    use -D ENABLE_PCH=ON to use it

---
+++ ImageLounge/src/prefix.h
@@ -0,0 +1,62 @@
+#pragma once
+
+//
+// precompiled header for nomacs
+//
+// speed up development builds by compiling
+// and inserting this header before all other headers
+//
+// for fast builds must also disable plugins and use Debug target
+//
+
+// test if its working
+// #error pch
+
+// this list of headers is by no means optimal for all cases
+// if too many headers go here then the build slows down
+
+// this was built mainly by looking at header used by a lot of files
+#if 1
+
+// DkUtils.h / DkSettings.h
+#include <assert.h>
+#include <functional>
+#include <memory>
+
+#ifndef Q_OS_WIN
+#include <time.h>
+#endif
+
+#ifdef WITH_OPENCV
+#ifdef Q_OS_WIN
+#pragma warning(disable : 4996)
+#endif
+
+#include "opencv2/core/core.hpp"
+#endif // WITH_OPENCV
+
+#include <QAction>
+#include <QApplication>
+#include <QBitArray>
+#include <QColor>
+#include <QDate>
+#include <QDebug>
+#include <QFileInfo>
+#include <QImage>
+#include <QMap>
+#include <QObject>
+#include <QRegularExpression>
+#include <QSettings>
+#include <QSharedMemory>
+#include <QSharedPointer>
+#include <QStringList>
+#include <QVector>
+
+// DkBaseWidgets.h
+#include <QDockWidget>
+#include <QLabel>
+#include <QScrollArea>
+#include <QTimer>
+#include <QWidget>
+
+#endif
