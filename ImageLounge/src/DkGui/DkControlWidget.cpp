6b35ada07fb9244987a1705efa678d743e88988b
Scrubs <scrubbbbs@gmail.com>
2025-03-27 08:10:02 -0500

    feat: disable widget animation when switching modes
    prevents glitch where layout changes shortly after switch due to widgets
    becoming hidden, which does not happen until animation ends, which is
    some time after the switch finishes
    
    this also disables animation for the initial startup of nomacs; as the
    window manager is probably fading in the window anyways we don't want to
    pile on more animations.

--- ImageLounge/src/DkGui/DkControlWidget.cpp
+++ ImageLounge/src/DkGui/DkControlWidget.cpp
@@ -752,7 +752,14 @@ void DkControlWidget::updateRating(int rating)
 
 void DkControlWidget::imagePresenceChanged(bool imagePresent)
 {
+    (void)imagePresent;
+
+    // disable animations while building initial view or image is lost
+    DkSettingsManager::param().display().suspendWidgetAnimation = true;
+
     showWidgetsSettings();
+
+    DkSettingsManager::param().display().suspendWidgetAnimation = false;
 }
 
 void DkControlWidget::setFullScreen(bool fullscreen)
