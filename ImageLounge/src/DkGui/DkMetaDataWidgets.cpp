31461620aca01786039f807973d74f3aafb9974f
Scrubs <scrubbbbs@gmail.com>
2025-03-27 09:46:36 -0500

    fix: improper override of DkFadeMixin::setVisible()
    There are some pitfalls when overriding DkFadeMixin::setVisible() due to
    the recursion through QWidget::setVisible, see comment @
    DkFadeMixin::setVisible.
    
    Infinite recursion is not possible, but the function body may be
    executed multiple times per call to setVisible()
    
    Currently, none of this is strictly required but in the future it would
    likely result in bugs.

--- ImageLounge/src/DkGui/DkMetaDataWidgets.cpp
+++ ImageLounge/src/DkGui/DkMetaDataWidgets.cpp
@@ -1051,6 +1051,8 @@ void DkMetaDataHUD::contextMenuEvent(QContextMenuEvent *event)
 void DkMetaDataHUD::setVisible(bool visible, bool saveSetting /* = true */)
 {
     DkFadeWidget::setVisible(visible, saveSetting);
+    if (mSetWidgetVisible)
+        return; // prevent recursion via fade()
 
     updateMetaData(mMetaData);
 }
