5c480258a32b9c0b06b7a85016d5466a5785fc64
Tzu-Yu Lee <leejuyuu@gmail.com>
2025-04-05 06:29:28 +0800

    fix: fix current index not found for zip
    Fix DkFilePreview current index not found when viewing zip files. Images
    inside zip files originally have encoded file path. However,
    `DkImageContainerT::checkForFileUpdates` later replace the file path
    with the path inside the archive, causing the path comparison to always
    fail.
    
    Record a copy of the original path that is not mutated inside
    DkImageContainer to fix this.

--- ImageLounge/src/DkGui/DkThumbsWidgets.cpp
+++ ImageLounge/src/DkGui/DkThumbsWidgets.cpp
@@ -812,7 +812,7 @@ void DkFilePreview::setFileInfo(QSharedPointer<DkImageContainerT> cImage)
     int tIdx = -1;
 
     for (int idx = 0; idx < mFilePaths.size(); idx++) {
-        if (mFilePaths.at(idx) == cImage->filePath()) {
+        if (mFilePaths.at(idx) == cImage->originalFilePath()) {
             tIdx = idx;
             break;
         }
@@ -829,7 +829,7 @@ void DkFilePreview::updateThumbs(QVector<QSharedPointer<DkImageContainerT>> thum
     mThumbs.clear();
     mFilePaths = std::vector<QString>(thumbs.size());
     for (int idx = 0; idx < thumbs.size(); idx++) {
-        mFilePaths[idx] = thumbs[idx]->filePath();
+        mFilePaths[idx] = thumbs[idx]->originalFilePath();
         if (thumbs.at(idx)->isSelected()) {
             currentFileIdx = idx;
         }
