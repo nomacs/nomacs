1ecdde93f95729b20976f3eee126444127477d23
Scrubs <scrubbbbs@gmail.com>
2025-04-28 14:39:32 -0500

    refactor: rename DkGui/DkFileInfo
    DkCore/DkFileInfo is the new class which is unrelated

--- ImageLounge/src/DkGui/DkWidgets.h
+++ ImageLounge/src/DkGui/DkWidgets.h
@@ -653,11 +653,11 @@ private:
     QMenu *mContextMenu = 0;
 };
 
-class DkFileInfo
+class DkFileInfoWrapper
 {
 public:
-    DkFileInfo();
-    DkFileInfo(const QFileInfo &fileInfo);
+    DkFileInfoWrapper();
+    DkFileInfoWrapper(const QFileInfo &fileInfo);
 
     QString getFilePath() const;
     bool exists() const;
@@ -677,7 +677,7 @@ class DkFolderLabel : public QLabel
     Q_OBJECT
 
 public:
-    DkFolderLabel(const DkFileInfo &fileInfo, QWidget *parent = 0, Qt::WindowFlags f = Qt::WindowFlags());
+    DkFolderLabel(const DkFileInfoWrapper &fileInfo, QWidget *parent = 0, Qt::WindowFlags f = Qt::WindowFlags());
 
 signals:
     void loadFileSignal(const QString &) const;
@@ -685,7 +685,7 @@ signals:
 protected:
     void mousePressEvent(QMouseEvent *ev) override;
 
-    DkFileInfo fileInfo;
+    DkFileInfoWrapper fileInfo;
 };
 
 class DkDirectoryEdit : public QLineEdit
