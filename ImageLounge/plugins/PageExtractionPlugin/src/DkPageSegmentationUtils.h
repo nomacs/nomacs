1255dd5eef48639eb9be8e017de57eca86de14db
Scrubs <scrubbbbs@gmail.com>
2025-06-25 14:49:05 -0500

    formatting: run clang-format-18 on plugins

--- ImageLounge/plugins/PageExtractionPlugin/src/DkPageSegmentationUtils.h
+++ ImageLounge/plugins/PageExtractionPlugin/src/DkPageSegmentationUtils.h
@@ -47,7 +47,7 @@ public:
      **/
     DkBox()
         : uc()
-        , lc() { };
+        , lc(){};
 
     /**
      * Constructor.
@@ -111,7 +111,7 @@ public:
     /**
      * Default destructor.
      **/
-    ~DkBox() { };
+    ~DkBox(){};
 
     void getStorageBuffer(char **buffer, size_t &length) const
     {
@@ -368,7 +368,7 @@ public:
 
     DkIPoint()
         : x(0)
-        , y(0) { };
+        , y(0){};
 
     DkIPoint(int x, int y)
     {
@@ -390,7 +390,7 @@ public:
     DkIPoint ry;
     int in;
 
-    DkVertex() { };
+    DkVertex(){};
 
     DkVertex(DkIPoint ip, DkIPoint rx, DkIPoint ry)
     {
