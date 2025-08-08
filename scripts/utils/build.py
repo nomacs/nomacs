8394a01473205cdc6c1548f57665aaeac69fc354
Scrubs <scrubbbbs@gmail.com>
2025-08-08 14:13:58 -0500

    ci(windows): log cmake commands

--- scripts/utils/build.py
+++ scripts/utils/build.py
@@ -13,7 +13,6 @@ def build(config):
     print(config.name + " ----------------------------------------------")
 
     cmakeconfig = "cmake " + " ".join(config.cmake_args())
-
     print(cmakeconfig)
 
     # configure
@@ -28,13 +27,17 @@ def build(config):
         return
 
     # build release
-    sp.run("cmake --build %s --config Release %s -- -m" %
-           (config.builddir, " ".join(config.cmake_build_args())), check=True)
+    cmakebuild = "cmake --build %s --config Release %s -- -m" % \
+           (config.builddir, " ".join(config.cmake_build_args()))
+    print(cmakebuild)
+    sp.run(cmakebuild, check=True)
 
     # build debug?!
     if config.buildconfig.lower() == "debug":
-        sp.run("cmake --build %s --config Debug %s -- -m" %
-               (config.builddir, " ".join(config.cmake_build_args())), check=True)
+        cmakebuild = "cmake --build %s --config Debug %s -- -m" % \
+            (config.builddir, " ".join(config.cmake_build_args()))
+        print(cmakebuild)
+        sp.run(cmakebuild, check=True)
 
     # install
     if config.install:
