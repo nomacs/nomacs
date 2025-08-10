4a3bb0bb1363a622a7b6bfb6bac0347287e9a2d6
Tzu-Yu Lee <leejuyuu@gmail.com>
2025-08-11 01:35:01 +0800

    fix: erase xmp data with loop
    Resetting rating to 0 did not work after setting rating multiple times.
    For example, pressing 3, 4, and then 0 does not set the rating to 0. It
    becomes 3 instead. This is due to the Xmp data are kind of "appended",
    so when setting multiple times, the key could be duplicated.
    
    Use a loop to erase all matched keys instead of the first one.

--- ImageLounge/src/DkCore/DkMetaData.cpp
+++ ImageLounge/src/DkCore/DkMetaData.cpp
@@ -1247,15 +1247,18 @@ bool DkMetaDataT::setRating(int r)
         if (pos != exifData.end())
             exifData.erase(pos);
 
-        Exiv2::XmpKey key2 = Exiv2::XmpKey("Xmp.xmp.Rating");
-        auto pos2 = xmpData.findKey(key2);
-        if (pos2 != xmpData.end())
-            xmpData.erase(pos2);
-
-        key2 = Exiv2::XmpKey("Xmp.MicrosoftPhoto.Rating");
-        pos2 = xmpData.findKey(key2);
-        if (pos2 != xmpData.end())
-            xmpData.erase(pos2);
+        // Xmp data is add instead of set, so the key could be duplicated.
+        constexpr std::string_view xmpK1 = "Xmp.xmp.Rating";
+        constexpr std::string_view xmpK2 = "Xmp.MicrosoftPhoto.Rating";
+        auto it = xmpData.begin();
+        while (it != xmpData.end()) {
+            const std::string k = it->key();
+            if (k != xmpK1 && k != xmpK2) {
+                it++;
+                continue;
+            }
+            it = xmpData.erase(it);
+        }
     }
 
     try {
