15b9b117f849fa5160538cef732658a1506b505b
Scrubs <scrubbbbs@gmail.com>
2025-10-12 09:41:00 -0500

    fix(thumbs): duplicate updates of thumb view
    Remove some updates of thumb view which are not necessary.

--- ImageLounge/src/DkGui/DkThumbsWidgets.cpp
+++ ImageLounge/src/DkGui/DkThumbsWidgets.cpp
@@ -2195,7 +2195,6 @@ void DkThumbScrollWidget::setVisible(bool visible)
     connectToActions(visible);
 
     if (visible) {
-        mThumbsScene->updateThumbLabels();
         mFilterEdit->setText("");
     } else
         mThumbsScene->cancelLoading();
