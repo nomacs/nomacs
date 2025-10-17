5d22a9377cb0234aa49997fe104b3c64701688c1
Scrubs <scrubbbbs@gmail.com>
2025-10-17 08:07:04 -0500

    feat(translation): also clean files when updating
    Clean ts files when updating to minimize the change set

--- ImageLounge/translations/src/update-translation.sh
+++ ImageLounge/translations/src/update-translation.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-lupdate  ../../src/ -no-obsolete -ts nomacs.ts > update.log
+lupdate  ../../src/ -no-obsolete -ts nomacs.ts > update.log || exit 1
 
 cd ..
 for f in *.ts; do
-    lupdate ../src/ -no-obsolete -ts "$f" >> src/update.log
+    lupdate ../src/ -no-obsolete -ts "$f" >> src/update.log || exit 2
+    ./src/clean-unfinished.py "$f" "$f" >> src/update.log  || exit 3
 done
 
-echo translation files updated, check update.log
+echo $0: translation files updated, check update.log
