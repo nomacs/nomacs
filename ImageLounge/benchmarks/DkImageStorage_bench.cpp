c5cb3af30c804a8fb25f6083780c873d600046a8
Scrubs <scrubbbbs@gmail.com>
2026-02-19 14:41:32 -0700

    fix(benchmark): rotation benchmark
    fix for refactored rotation, add some angles and test different image
    sizes

--- ImageLounge/benchmarks/DkImageStorage_bench.cpp
+++ ImageLounge/benchmarks/DkImageStorage_bench.cpp
@@ -1,27 +1,73 @@
 #include "../src/DkCore/DkImageStorage.h"
 #include <benchmark/benchmark.h>
 
-const int ROTATE_ANGLE = 0;
-const auto IMAGE_PATH = "../ImageLounge/src/img/splash-screen.png";
-
-static void BM_RotateImageFast(benchmark::State &state)
-{
-    QImage img = QImage(IMAGE_PATH);
-    QImage res{};
-    for (auto _ : state) {
-        res = nmc::rotateImageFast(img, ROTATE_ANGLE);
-    }
-}
-BENCHMARK(BM_RotateImageFast);
-
 static void BM_RotateImage(benchmark::State &state)
 {
-    QImage img = QImage(IMAGE_PATH);
-    QImage res{};
+    int cacheLevel = state.range(0);
+    int bytesPerPixel = 4;
+    auto &cpu = benchmark::CPUInfo::Get();
+    int cacheSize = 0;
+    if (cacheLevel < 4) {
+        for (auto cache : cpu.caches) {
+            if (cache.level == cacheLevel) {
+                cacheSize = cache.size;
+                break;
+            }
+        }
+    }
+    if (cacheLevel == 4) {
+        cacheSize = 128 * 1024 * 1024;
+    }
+    if (cacheSize == 0) {
+        state.SkipWithMessage("No CPU cache at this level?"); // no L3 cache, probably
+    }
+
+    int dim = sqrt(cacheSize / bytesPerPixel);
+    QImage img{dim, dim, QImage::Format_ARGB32};
+
+    state.SetLabel(QString().asprintf("%dKB %dx%d", cacheSize / 1024, dim, dim).toStdString());
+
+    double angle = state.range(1);
     for (auto _ : state) {
-        res = nmc::rotateImage(img, ROTATE_ANGLE);
+        benchmark::DoNotOptimize(nmc::DkImage::rotateImage(img, angle));
     }
 }
-BENCHMARK(BM_RotateImage);
+BENCHMARK(BM_RotateImage)
+    // L1 Cache
+    ->Args({1, 0})
+    ->Args({1, 90})
+    ->Args({1, 180})
+    ->Args({1, 270})
+    ->Args({1, 45})
+    ->Args({1, 135})
+    ->Args({1, 225})
+    ->Args({1, 315})
+    // L2 Cache
+    ->Args({2, 0})
+    ->Args({2, 90})
+    ->Args({2, 180})
+    ->Args({2, 270})
+    ->Args({2, 45})
+    ->Args({2, 135})
+    ->Args({2, 225})
+    ->Args({2, 315})
+    // L3 cache
+    ->Args({3, 0})
+    ->Args({3, 90})
+    ->Args({3, 180})
+    ->Args({3, 270})
+    ->Args({3, 45})
+    ->Args({3, 135})
+    ->Args({3, 225})
+    ->Args({3, 315})
+    // Main Memory
+    ->Args({4, 0})
+    ->Args({4, 90})
+    ->Args({4, 180})
+    ->Args({4, 270})
+    ->Args({4, 45})
+    ->Args({4, 135})
+    ->Args({4, 225})
+    ->Args({4, 315});
 
 BENCHMARK_MAIN();
