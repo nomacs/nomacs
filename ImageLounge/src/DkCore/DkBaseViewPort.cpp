e5dc9365aa0b483d8a25a66661eeada21ccc011d
Scrubs <scrubbbbs@gmail.com>
2026-02-20 06:08:32 -0700

    fix: Qt 6.2 support
    No QRect::toRectF()

--- ImageLounge/src/DkCore/DkBaseViewPort.cpp
+++ ImageLounge/src/DkCore/DkBaseViewPort.cpp
@@ -522,7 +522,7 @@ DkBaseViewPort::RenderParams DkBaseViewPort::getRenderParams(double devicePixelR
     QRect deviceRect = tx.mapRect(viewRect).toRect();
 
     // Rect to draw image, with world matrix disabled
-    viewRect = tx.inverted().mapRect(deviceRect.toRectF());
+    viewRect = tx.inverted().mapRect(QRectF{deviceRect});
 
     // Rect to draw image, with world matrix enabled
     QRectF dstRect = worldMatrix.inverted().mapRect(viewRect);
