nomacs/nomacs

commit 1b42f6815adbd73177a2af9b7a12414c1577cc1b

fix(frameless): splash screen buttons invoked twice
Eat the event to prevent this

ImageLounge/src/DkGui/DkViewPort.cpp
@@ -2300,14 +2300,15 @@ void DkViewPortFrameless::mouseReleaseEvent(QMouseEvent *event)
 
         for (int idx = 0; idx < mStartActionsRects.size(); idx++) {
             if (mStartActionsRects[idx].contains(pos)) {
+                event->accept();
+                unsetCursor();
+                // setCursor(Qt::OpenHandCursor);
                 mStartActions[idx]->trigger();
-                break;
+                return;
             }
         }
     }
 
-    unsetCursor();
-    // setCursor(Qt::OpenHandCursor);
     DkViewPort::mouseReleaseEvent(event);
 }
 

commit 3e0e61386a5b1fc013db75ddeaf50fb9927fc2c4

fix: fix frameless cursor not reset after pan
1b42f68 accidentally removed `unsetCursor()` call from one code path,
which makes the cursor not reset after panning.

Add the call back to fix this.

ImageLounge/src/DkGui/DkViewPort.cpp
@@ -2164,6 +2164,7 @@ void DkViewPortFrameless::mouseReleaseEvent(QMouseEvent *event)
         }
     }
 
+    unsetCursor();
     DkViewPort::mouseReleaseEvent(event);
 }
 